9b827eb347d8bdac51b21a2bdc543e2f21469701
Joyce Lee <41254055-joycelee20@users.noreply.replit.com>
2025-04-17 01:47:20 +0000

    Restored to 'dd746a4a98aea805e0a017f130f452ba76252f4f'
    Replit-Restored-To: dd746a4a98aea805e0a017f130f452ba76252f4f

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,7 @@
-
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Route, Switch } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { Toaster } from "@/components/ui/toaster";
-import { ThemeProvider } from "@/components/theme-provider";
 
 // Import pages
 import Dashboard from "@/pages/dashboard";
@@ -28,12 +26,10 @@ function Router() {
 
 function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="ui-theme">
-      <QueryClientProvider client={queryClient}>
-        <Router />
-        <Toaster />
-      </QueryClientProvider>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <Router />
+      <Toaster />
+    </QueryClientProvider>
   );
 }
 
